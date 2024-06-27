974eb885a2b97fe25831ec97919694fa90a552b2
Farhan Ali <83398139+htsimle@users.noreply.github.com>
2024-06-27 21:27:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // Your frontend URL
+  origin: 'https://tm-frontend-mauve.vercel.app', // Adjust this to your frontend URL on Vercel
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
@@ -19,7 +19,7 @@ app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
 
 sequelize.sync().then(() => {
-  app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+  app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is running on port', process.env.PORT || 5000);
   });
 });
